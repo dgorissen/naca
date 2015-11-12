f5cbb7c32ba41175852b39a39ff72da49baf4f1e
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2015-11-12 23:18:33 +0100

    [master] Major refactoring, add a main function and a demo

--- naca.py
+++ naca.py
@@ -26,12 +26,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-import math
+from math import cos, sin, tan
+from math import atan
+from math import pi
+from math import pow
+from math import sqrt
 
 def linspace(start,stop,np):
-"""
-Emulate Matlabs linspace
-"""
+    """
+    Emulate Matlab linspace
+    """
 
     delta = (stop - start) / (np - 1.0)
 
@@ -41,12 +45,12 @@ Emulate Matlabs linspace
 
     return p
 
-def Interpolate(xa,ya,queryPoints):
-"""
-A cubic spline interpolation on a given set of points (x,y)
-Recalculates everything on every call which is far from efficient but does the job for now
-should eventually be replaced by an external helper class
-"""
+def interpolate(xa,ya,queryPoints):
+    """
+    A cubic spline interpolation on a given set of points (x,y)
+    Recalculates everything on every call which is far from efficient but does the job for now
+    should eventually be replaced by an external helper class
+    """
 
     # PreCompute() from Paint Mono which in turn adapted:
     # NUMERICAL RECIPES IN C: THE ART OF SCIENTIFIC COMPUTING
@@ -57,9 +61,6 @@ should eventually be replaced by an external helper class
     n = len(xa)
     u = [0]*n
     y2 = [0]*n
-
-    u[0] = 0
-    y2[0] = 0
 
     for i in range(1,n-1):
 
@@ -84,7 +85,7 @@ should eventually be replaced by an external helper class
     for i in range(n-2,-1,-1):
         y2[i] = y2[i] * y2[i + 1] + u[i]
 
-    # Interpolate() adapted from Paint Mono which in turn adapted:
+    # interpolate() adapted from Paint Mono which in turn adapted:
     # NUMERICAL RECIPES IN C: THE ART OF SCIENTIFIC COMPUTING
     # ISBN 0-521-43108-5, page 113, section 3.3.
     # http://paint-mono.googlecode.com/svn/trunk/src/PdnLib/SplineInterpolator.cs
@@ -118,34 +119,34 @@ should eventually be replaced by an external helper class
     return results
 
 def naca4(number, n, finite_TE=False, half_cosine_spacing=False):
-"""
-Returns n points (for EACH HALF) in [0 1] for the given 4 digit NACA number string
-"""
+    """
+    Returns n points (for EACH HALF) in [0 1] for the given 4 digit NACA number string
+    """
 
     m = float(number[0])/100.0
     p = float(number[1])/10.0
     t = float(number[2:])/100.0
 
-    a0 = 0.2969
+    a0 = +0.2969
     a1 = -0.1260
     a2 = -0.3516
-    a3 = 0.2843
+    a3 = +0.2843
 
     if finite_TE:
         a4 = -0.1015 # For finite thick TE
     else:
-        a4 = -0.1036  # For zero thick TE
+        a4 = -0.1036 # For zero thick TE
 
     if half_cosine_spacing:
-        beta = linspace(0.0,math.pi,n+1)
-        x = map(lambda xx : (0.5*(1.0-math.cos(xx))),beta)  # Half cosine based spacing
+        beta = linspace(0.0,pi,n+1)
+        x = [(0.5*(1.0-cos(xx))) for xx in beta]  # Half cosine based spacing
     else:
         x = linspace(0.0,1.0,n+1)
 
-    yt = map(lambda xx: (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x)
+    yt = [(t/0.2)*(a0*sqrt(xx)+a1*xx+a2*pow(xx,2)+a3*pow(xx,3)+a4*pow(xx,4)) for xx in x]
 
-    xc1 = filter(lambda xx: xx <= p, x)
-    xc2 = filter(lambda xx: xx > p, x)
+    xc1 = [xx for xx in x if xx <= p]
+    xc2 = [xx for xx in x if xx > p]
     xc = xc1 + xc2
 
     if p == 0:
@@ -157,33 +158,31 @@ Returns n points (for EACH HALF) in [0 1] for the given 4 digit NACA number stri
 
         zc = [0]*len(xc)
     else:
-        yc1 = map(lambda xx: (m/math.pow(p,2))*(2*p*xx-math.pow(xx,2)),xc1)
-        yc2 = map(lambda xx: (m/math.pow((1-p),2))*((1-2*p)+2*p*xx-math.pow(xx,2)),xc2)
+        yc1 = [(m/pow(p,2))*(2*p*xx-pow(xx,2)) for xx in xc1]
+        yc2 = [(m/pow((1-p),2))*((1-2*p)+2*p*xx-pow(xx,2)) for xx in xc2]
         zc = yc1 + yc2
 
-        dyc1_dx = map(lambda xx: (m/math.pow(p,2))*(2*p-2*xx),xc1)
-        dyc2_dx = map(lambda xx: (m/math.pow((1-p),2))*(2*p-2*xx),xc2)
+        dyc1_dx = [(m/pow(p,2))*(2*p-2*xx) for xx in xc1]
+        dyc2_dx = [(m/pow((1-p),2))*(2*p-2*xx) for xx in xc2]
         dyc_dx = dyc1_dx + dyc2_dx
 
-        theta = [math.atan(xx) for xx in dyc_dx]
+        theta = [atan(xx) for xx in dyc_dx]
 
-        xu = map(lambda xx,yy,zz: xx-yy * math.sin(zz),x,yt,theta)
-        yu = map(lambda xx,yy,zz: xx+yy * math.cos(zz),zc,yt,theta)
+        xu = [xx - yy * sin(zz) for xx,yy,zz in zip(x,yt,theta)]
+        yu = [xx + yy * cos(zz) for xx,yy,zz in zip(zc,yt,theta)]
 
-        xl = map(lambda xx,yy,zz: xx + yy * math.sin(zz),x,yt,theta)
-        yl = map(lambda xx,yy,zz: xx - yy * math.cos(zz),zc,yt,theta)
+        xl = [xx + yy * sin(zz) for xx,yy,zz in zip(x,yt,theta)]
+        yl = [xx - yy * cos(zz) for xx,yy,zz in zip(zc,yt,theta)]
 
     X = xu[::-1] + xl[1:]
     Z = yu[::-1] + yl[1:]
 
-    pts = zip(X,Z)
-
-    return pts
+    return X,Z
 
 def naca5(number, n, finite_TE=False, half_cosine_spacing=False):
-"""
-Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number string
-"""
+    """
+    Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number string
+    """
 
     naca1 = int(number[0])
     naca23 = int(number[1:3])
@@ -199,27 +198,27 @@ Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number stri
     a3 = 0.2843
 
     if finite_TE:
-        a4 = -0.1015; # For finite thickness trailing edge
+        a4 = -0.1015 # For finite thickness trailing edge
     else:
-        a4 = -0.1036;  # For zero thickness trailing edge
+        a4 = -0.1036  # For zero thickness trailing edge
 
     if half_cosine_spacing:
-        beta = linspace(0.0,math.pi,n+1)
-        x = map(lambda x : (0.5*(1.0-math.cos(x))),beta)  # Half cosine based spacing
+        beta = linspace(0.0,pi,n+1)
+        x = [(0.5*(1.0-cos(x))) for x in beta]  # Half cosine based spacing
     else:
         x = linspace(0.0,1.0,n+1)
 
-    yt = map(lambda xx : (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x)
+    yt = [(t/0.2)*(a0*sqrt(xx)+a1*xx+a2*pow(xx,2)+a3*pow(xx,3)+a4*pow(xx,4)) for xx in x]
 
     P = [0.05,0.1,0.15,0.2,0.25]
     M = [0.0580,0.1260,0.2025,0.2900,0.3910]
     K = [361.4,51.64,15.957,6.643,3.230]
 
-    m = Interpolate(P,M,[p])[0]
-    k1 = Interpolate(M,K,[m])[0]
+    m = interpolate(P,M,[p])[0]
+    k1 = interpolate(M,K,[m])[0]
 
-    xc1 = filter(lambda xx: xx <= m, x)
-    xc2 = filter(lambda xx: xx > m, x)
+    xc1 = [xx for xx in x if xx <= p]
+    xc2 = [xx for xx in x if xx > p]
     xc = xc1 + xc2
 
     if p == 0:
@@ -231,34 +230,64 @@ Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number stri
 
         zc = [0]*len(xc)
     else:
-        yc1 = map(lambda xx : (1.0/6.0)*k1*( math.pow(xx,3)-3*m*math.pow(xx,2)+ math.pow(m,2)*(3-m)*xx),xc1)
-        yc2 = map(lambda xx : (1.0/6.0)*k1*math.pow(m,3)*(1-xx),xc2)
-        zc = map(lambda xx : (cld/0.3) * xx, yc1 + yc2)
+        yc1 = [(1.0/6.0)*k1*( pow(xx,3)-3*m*pow(xx,2)+ pow(m,2)*(3-m)*xx) for xx in xc1]
+        yc2 = [(1.0/6.0)*k1*pow(m,3)*(1-xx) for xx in xc2]
+        zc = [cld / 0.3 * xx for xx in yc1 + yc2]
 
-        dyc1_dx = map(lambda xx : cld/0.3*(1.0/6.0)*k1*( 3*math.pow(xx,2)-6*m*xx+math.pow(m,2)*(3-m) ), xc1)
-        dyc2_dx = [cld/0.3*(1.0/6.0)*k1*math.pow(m,3)]*len(xc2)
+        dyc1_dx = [cld/0.3*(1.0/6.0)*k1*( 3*pow(xx,2)-6*m*xx+pow(m,2)*(3-m)) for xx in xc1]
+        dyc2_dx = [cld/0.3*(1.0/6.0)*k1*pow(m,3)]*len(xc2)
 
         dyc_dx = dyc1_dx + dyc2_dx
-        theta = [math.atan(xx) for xx in dyc_dx]
+        theta = [atan(xx) for xx in dyc_dx]
 
-        xu = map(lambda xx,yy,zz: xx - yy * math.sin(zz),x,yt,theta)
-        yu = map(lambda xx,yy,zz: xx + yy * math.cos(zz),zc,yt,theta)
+        xu = [xx - yy * sin(zz) for xx,yy,zz in zip(x,yt,theta)]
+        yu = [xx + yy * cos(zz) for xx,yy,zz in zip(zc,yt,theta)]
 
-        xl = map(lambda xx,yy,zz: xx + yy * math.sin(zz),x,yt,theta)
-        yl = map(lambda xx,yy,zz: xx - yy * math.cos(zz),zc,yt,theta)
+        xl = [xx + yy * sin(zz) for xx,yy,zz in zip(x,yt,theta)]
+        yl = [xx - yy * cos(zz) for xx,yy,zz in zip(zc,yt,theta)]
+
 
     X = xu[::-1] + xl[1:]
     Z = yu[::-1] + yl[1:]
 
-    pts = zip(X,Z)
+    return X,Z
 
-    return pts
+def naca(number, n, finite_TE=False, half_cosine_spacing=False):
+    if len(number)==4:
+        return naca4(number, n, finite_TE, half_cosine_spacing)
+    elif len(number)==5:
+        return naca5(number, n, finite_TE, half_cosine_spacing)
+    else:
+        raise Exception
+
+def demo(profNaca = ['0009', '2414', '6409'], nPoints = 240):
+    #profNaca = ['0009', '0012', '2414', '2415', '6409' , '0006', '0008', '0010', '0012', '0015']
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots()
+    plt.axis('equal')
+    ax.grid(True)
+    hp = [None]*len(profNaca)
+    for i,p in enumerate(profNaca):
+        X,Y = naca(p, nPoints)
+        hp[i], = plt.plot(X, Y, '-', linewidth = 1, label = p)
+    plt.axis((-0.1,1.1)+plt.axis()[2:])
+    ax.legend(hp, profNaca)
+    plt.show()
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description = \
+        'Script to create NACA4 and NACA5 profiles.' \
+        'If no argument is provided, a demo is displayed')
+    parser.add_argument('-p','--profile')
+    parser.add_argument('-n','--nbPoints', type = int, default = 120)
+    args = parser.parse_args()
+    if args.profile is None:
+        demo(nPoints = args.nbPoints)
+    else:
+        X,Y = naca(args.profile, args.nbPoints)
+        for x,y in zip(X,Y):
+            print(x,y)
 
 if __name__ == "__main__":
-
-    # Examples
-    pts = naca4("0012",60)
-    #pts = naca5("23015",60)
-
-    for p in pts:
-        print p[0],p[1]
+    main()
