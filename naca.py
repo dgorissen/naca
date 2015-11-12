294cdf0e33782712c7d8c5afdcf471bd2d517c52
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2015-11-12 21:17:28 +0100

    [master] Removed trailing ;

--- naca.py
+++ naca.py
@@ -58,8 +58,8 @@ should eventually be replaced by an external helper class
     u = [0]*n
     y2 = [0]*n
 
-    u[0] = 0;
-    y2[0] = 0;
+    u[0] = 0
+    y2[0] = 0
 
     for i in range(1,n-1):
 
@@ -74,22 +74,22 @@ should eventually be replaced by an external helper class
 
         ddydx = (ya[i + 1] - ya[i]) / (xa[i + 1] - xa[i]) - (ya[i] - ya[i - 1]) / (xa[i] - xa[i - 1])
 
-        u[i] = (6.0 * ddydx / wx - sig * u[i - 1]) / p;
+        u[i] = (6.0 * ddydx / wx - sig * u[i - 1]) / p
 
 
-    y2[n - 1] = 0;
+    y2[n - 1] = 0
 
     # This is the backsubstitution loop of the tridiagonal algorithm
     #((int i = n - 2; i >= 0; --i):
     for i in range(n-2,-1,-1):
-        y2[i] = y2[i] * y2[i + 1] + u[i];
+        y2[i] = y2[i] * y2[i + 1] + u[i]
 
     # Interpolate() adapted from Paint Mono which in turn adapted:
     # NUMERICAL RECIPES IN C: THE ART OF SCIENTIFIC COMPUTING
     # ISBN 0-521-43108-5, page 113, section 3.3.
     # http://paint-mono.googlecode.com/svn/trunk/src/PdnLib/SplineInterpolator.cs
 
-    results = [0]*n;
+    results = [0]*n
 
     #loop over all query points
     for i in range(len(queryPoints)):
@@ -98,38 +98,38 @@ should eventually be replaced by an external helper class
         # are in order, and closely spaced, one would do better
         # to store previous values of klo and khi and test if
 
-        klo = 0;
-        khi = n - 1;
+        klo = 0
+        khi = n - 1
 
         while (khi - klo > 1):
-            k = (khi + klo) >> 1;
+            k = (khi + klo) >> 1
             if (xa[k] > queryPoints[i]):
-                khi = k;
+                khi = k
             else:
-                klo = k;
+                klo = k
 
-        h = xa[khi] - xa[klo];
-        a = (xa[khi] - queryPoints[i]) / h;
-        b = (queryPoints[i] - xa[klo]) / h;
+        h = xa[khi] - xa[klo]
+        a = (xa[khi] - queryPoints[i]) / h
+        b = (queryPoints[i] - xa[klo]) / h
 
         # Cubic spline polynomial is now evaluated.
-        results[i] = a * ya[klo] + b * ya[khi] + ((a * a * a - a) * y2[klo] + (b * b * b - b) * y2[khi]) * (h * h) / 6.0;
+        results[i] = a * ya[klo] + b * ya[khi] + ((a * a * a - a) * y2[klo] + (b * b * b - b) * y2[khi]) * (h * h) / 6.0
 
-    return results;
+    return results
 
 def naca4(number, n, finite_TE=False, half_cosine_spacing=False):
 """
 Returns n points (for EACH HALF) in [0 1] for the given 4 digit NACA number string
 """
 
-    m = float(number[0])/100.0;
-    p = float(number[1])/10.0;
-    t = float(number[2:])/100.0;
+    m = float(number[0])/100.0
+    p = float(number[1])/10.0
+    t = float(number[2:])/100.0
 
-    a0 = 0.2969;
-    a1 = -0.1260;
-    a2 = -0.3516;
-    a3 = 0.2843;
+    a0 = 0.2969
+    a1 = -0.1260
+    a2 = -0.3516
+    a3 = 0.2843
 
     if finite_TE:
         a4 = -0.1015 # For finite thick TE
@@ -142,36 +142,36 @@ Returns n points (for EACH HALF) in [0 1] for the given 4 digit NACA number stri
     else:
         x = linspace(0.0,1.0,n+1)
 
-    yt = map(lambda xx: (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x);
+    yt = map(lambda xx: (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x)
 
-    xc1 = filter(lambda xx: xx <= p, x);
-    xc2 = filter(lambda xx: xx > p, x);
-    xc = xc1 + xc2;
+    xc1 = filter(lambda xx: xx <= p, x)
+    xc2 = filter(lambda xx: xx > p, x)
+    xc = xc1 + xc2
 
     if p == 0:
-        xu = x;
-        yu = yt;
+        xu = x
+        yu = yt
 
-        xl = x;
-        yl = [-xx for xx in yt];
+        xl = x
+        yl = [-xx for xx in yt]
 
         zc = [0]*len(xc)
     else:
-        yc1 = map(lambda xx: (m/math.pow(p,2))*(2*p*xx-math.pow(xx,2)),xc1);
-        yc2 = map(lambda xx: (m/math.pow((1-p),2))*((1-2*p)+2*p*xx-math.pow(xx,2)),xc2);
+        yc1 = map(lambda xx: (m/math.pow(p,2))*(2*p*xx-math.pow(xx,2)),xc1)
+        yc2 = map(lambda xx: (m/math.pow((1-p),2))*((1-2*p)+2*p*xx-math.pow(xx,2)),xc2)
         zc = yc1 + yc2
 
-        dyc1_dx = map(lambda xx: (m/math.pow(p,2))*(2*p-2*xx),xc1);
-        dyc2_dx = map(lambda xx: (m/math.pow((1-p),2))*(2*p-2*xx),xc2);
-        dyc_dx = dyc1_dx + dyc2_dx;
+        dyc1_dx = map(lambda xx: (m/math.pow(p,2))*(2*p-2*xx),xc1)
+        dyc2_dx = map(lambda xx: (m/math.pow((1-p),2))*(2*p-2*xx),xc2)
+        dyc_dx = dyc1_dx + dyc2_dx
 
-        theta = [math.atan(xx) for xx in dyc_dx];
+        theta = [math.atan(xx) for xx in dyc_dx]
 
-        xu = map(lambda xx,yy,zz: xx-yy * math.sin(zz),x,yt,theta);
-        yu = map(lambda xx,yy,zz: xx+yy * math.cos(zz),zc,yt,theta);
+        xu = map(lambda xx,yy,zz: xx-yy * math.sin(zz),x,yt,theta)
+        yu = map(lambda xx,yy,zz: xx+yy * math.cos(zz),zc,yt,theta)
 
-        xl = map(lambda xx,yy,zz: xx + yy * math.sin(zz),x,yt,theta);
-        yl = map(lambda xx,yy,zz: xx - yy * math.cos(zz),zc,yt,theta);
+        xl = map(lambda xx,yy,zz: xx + yy * math.sin(zz),x,yt,theta)
+        yl = map(lambda xx,yy,zz: xx - yy * math.cos(zz),zc,yt,theta)
 
     X = xu[::-1] + xl[1:]
     Z = yu[::-1] + yl[1:]
@@ -185,18 +185,18 @@ def naca5(number, n, finite_TE=False, half_cosine_spacing=False):
 Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number string
 """
 
-    naca1 = int(number[0]);
-    naca23 = int(number[1:3]);
-    naca45 = int(number[3:]);
+    naca1 = int(number[0])
+    naca23 = int(number[1:3])
+    naca45 = int(number[3:])
 
-    cld = naca1*(3.0/2.0)/10.0;
-    p = 0.5*naca23/100.0;
-    t = naca45/100.0;
+    cld = naca1*(3.0/2.0)/10.0
+    p = 0.5*naca23/100.0
+    t = naca45/100.0
 
-    a0 =  0.2969;
-    a1 = -0.1260;
-    a2 = -0.3516;
-    a3 = 0.2843;
+    a0 =  0.2969
+    a1 = -0.1260
+    a2 = -0.3516
+    a3 = 0.2843
 
     if finite_TE:
         a4 = -0.1015; # For finite thickness trailing edge
@@ -209,7 +209,7 @@ Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number stri
     else:
         x = linspace(0.0,1.0,n+1)
 
-    yt = map(lambda xx : (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x);
+    yt = map(lambda xx : (t/0.2)*(a0*math.sqrt(xx)+a1*xx+a2*math.pow(xx,2)+a3*math.pow(xx,3)+a4*math.pow(xx,4)),x)
 
     P = [0.05,0.1,0.15,0.2,0.25]
     M = [0.0580,0.1260,0.2025,0.2900,0.3910]
@@ -218,16 +218,16 @@ Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number stri
     m = Interpolate(P,M,[p])[0]
     k1 = Interpolate(M,K,[m])[0]
 
-    xc1 = filter(lambda xx: xx <= m, x);
-    xc2 = filter(lambda xx: xx > m, x);
-    xc = xc1 + xc2;
+    xc1 = filter(lambda xx: xx <= m, x)
+    xc2 = filter(lambda xx: xx > m, x)
+    xc = xc1 + xc2
 
     if p == 0:
         xu = x
         yu = yt
 
         xl = x
-        yl = [-x for x in yt];
+        yl = [-x for x in yt]
 
         zc = [0]*len(xc)
     else:
@@ -235,20 +235,20 @@ Returns n points (for EACH HALF) in [0 1] for the given 5 digit NACA number stri
         yc2 = map(lambda xx : (1.0/6.0)*k1*math.pow(m,3)*(1-xx),xc2)
         zc = map(lambda xx : (cld/0.3) * xx, yc1 + yc2)
 
-        dyc1_dx = map(lambda xx : cld/0.3*(1.0/6.0)*k1*( 3*math.pow(xx,2)-6*m*xx+math.pow(m,2)*(3-m) ), xc1);
-        dyc2_dx = [cld/0.3*(1.0/6.0)*k1*math.pow(m,3)]*len(xc2);
+        dyc1_dx = map(lambda xx : cld/0.3*(1.0/6.0)*k1*( 3*math.pow(xx,2)-6*m*xx+math.pow(m,2)*(3-m) ), xc1)
+        dyc2_dx = [cld/0.3*(1.0/6.0)*k1*math.pow(m,3)]*len(xc2)
 
-        dyc_dx = dyc1_dx + dyc2_dx;
-        theta = [math.atan(xx) for xx in dyc_dx];
+        dyc_dx = dyc1_dx + dyc2_dx
+        theta = [math.atan(xx) for xx in dyc_dx]
 
-        xu = map(lambda xx,yy,zz: xx - yy * math.sin(zz),x,yt,theta);
-        yu = map(lambda xx,yy,zz: xx + yy * math.cos(zz),zc,yt,theta);
+        xu = map(lambda xx,yy,zz: xx - yy * math.sin(zz),x,yt,theta)
+        yu = map(lambda xx,yy,zz: xx + yy * math.cos(zz),zc,yt,theta)
 
         xl = map(lambda xx,yy,zz: xx + yy * math.sin(zz),x,yt,theta)
         yl = map(lambda xx,yy,zz: xx - yy * math.cos(zz),zc,yt,theta)
 
-    X = xu[::-1] + xl[1:];
-    Z = yu[::-1] + yl[1:];
+    X = xu[::-1] + xl[1:]
+    Z = yu[::-1] + yl[1:]
 
     pts = zip(X,Z)
 
